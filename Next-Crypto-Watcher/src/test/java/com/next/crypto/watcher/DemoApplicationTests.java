87a971cdf4c7e9742396472802deab8296c833c0
q1432960 <saurav.thapa@qvc.com>
2021-03-08 10:37:32 -0500

    code skeleton check in

---
+++ Next-Crypto-Watcher/src/test/java/com/next/crypto/watcher/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.next.crypto.watcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
